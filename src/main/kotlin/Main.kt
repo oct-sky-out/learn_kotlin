e5b0bcbac777080b43d81aa1de8d9e3c39e890a4
oct-sky-out <kds3335k@icloud.com>
2025-05-03 16:17:09 +0900

    1. 속성선언

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package com.oct_sky_out
 
+import java.math.BigDecimal
+
 fun main() {
-    print("Hello World!")
+    // NOTE: 코틀린은 세미콜론이 필요없습니다.
+    // NOTE: 코틀린은 자동으로 타입을 추론합니다. (자바 11의 var와 동일)
+
+    // TODO: val과 var를 이용한 변수를 선언합니다.
+
+    // NOTE: val은 불변 변수이고, var은 가변 변수를 선언 할 수 있는 예약어입니다.
+    val integerVariable = 1
+    var stringVariable = "stringVariable"
+
+//    val integerVariable = 1 Error
+    stringVariable += " hello"
+
+    val bigDecimalVariable = BigDecimal("12.1")
+    val addedBigDecimal = bigDecimalVariable.add(BigDecimal("0.2"))
+
+    println(integerVariable)
+    println(stringVariable)
+
+    println(bigDecimalVariable)
+    println(addedBigDecimal)
 }
