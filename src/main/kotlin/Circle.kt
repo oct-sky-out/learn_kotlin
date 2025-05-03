c0ba9efc9d0197db83e400289f462a49f8d364d5
oct-sky-out <kds3335k@icloud.com>
2025-05-03 16:43:24 +0900

    backing field 설명 추가

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -8,6 +8,7 @@ class Circle(radius : Int) {
 
     var radius = radius
         set(value) {
+            // NOTE: 커스텀 게터/세터 내에서 실제 저장된 값을 가리키는 백킹 필드(backing field)다.
             // field -> this.radius
             field = value
         }
