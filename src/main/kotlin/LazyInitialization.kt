fa61b3c615f1341e9ea851732de36faa32f33247
oct-sky-out <kds3335k@icloud.com>
2025-05-04 16:04:59 +0900

    6. lazy mode

--- src/main/kotlin/LazyInitialization.kt
+++ src/main/kotlin/LazyInitialization.kt
@@ -13,4 +13,14 @@ class LazyInitialization {
         mapOf("baseUrl" to baseUrl, "apiKey" to apiKey)
     }
 
+    val publicationConfig : Map<String, String> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        val baseUrl = "https://jsonplaceholder.typicode.com/"
+        val apiKey = UUID.randomUUID().toString()
+
+        println("call by config lazy scope : $baseUrl")
+        println("call by config lazy scope : $apiKey")
+
+        mapOf("baseUrl" to baseUrl, "apiKey" to apiKey)
+    }
+
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,10 +3,17 @@ package com.oct_sky_out
 fun main() {
 
     val lazyConfig = LazyInitialization()
+    // NOTE : lazy 모드에는 총 2가지가 존재하며, 기본은 SYNCHRONIZED 모드이다.
 
     // NOTE : 첫 사용 접근시 초기화. 따라서 lazyConfig.config map을 처음 초기화 할 때 내부의 print문이 동작.
     println("this is main scope : ${lazyConfig.config["baseUrl"]}")
 
     // NOTE : 두번째 사용시에는 리소스를 재로드하지않음. 캐싱된 값을 사용.
     println("this is main scope : ${lazyConfig.config["apiKey"]}")
+
+    // NOTE : PUBLICATION 모드로 동작하는 config
+    println("this is main scope : ${lazyConfig.publicationConfig["baseUrl"]}")
+
+    // NOTE : 두번째 사용시에는 리소스를 재로드하지않음. 캐싱된 값을 사용.
+    println("this is main scope : ${lazyConfig.publicationConfig["apiKey"]}")
 }
