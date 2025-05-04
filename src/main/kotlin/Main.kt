01728729e0b5749d71d050e51fadd8405de0391a
oct-sky-out <kds3335k@icloud.com>
2025-05-04 17:14:00 +0900

    7. class define

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,4 @@
 package com.oct_sky_out
 
 fun main() {
-
-    val hero = Hero("kotlin", level = 1)
-
-    // TODO : 히어로의 이름과 레벨을 변경해보세요.
-    //      밸리데이션의 정의대로 올바르지않다면, 에러가 발생합니다.
-    hero.name = "hero"
-    hero.level = 2
 }
