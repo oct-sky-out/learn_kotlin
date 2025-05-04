29ac6149458bd4543affc7e33946773d0f91cde4
oct-sky-out <kds3335k@icloud.com>
2025-05-04 15:37:30 +0900

    6. resource lazy load

---
+++ src/main/kotlin/LazyInitialization.kt
@@ -0,0 +1,16 @@
+package com.oct_sky_out
+
+import java.util.*
+
+class LazyInitialization {
+    val config : Map<String, String> by lazy {
+        val baseUrl = "https://jsonplaceholder.typicode.com/"
+        val apiKey = UUID.randomUUID().toString()
+
+        println("call by config lazy scope : $baseUrl")
+        println("call by config lazy scope : $apiKey")
+
+        mapOf("baseUrl" to baseUrl, "apiKey" to apiKey)
+    }
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
 package com.oct_sky_out
 
 fun main() {
-    IfControl().execute()
-    WhenControl().execute()
-    LoopControl().executeFor()
-    LoopControl().executeForCompositIndex()
-    LoopControl().executeForEach()
+
+    val lazyConfig = LazyInitialization()
+
+    // NOTE : 첫 사용 접근시 초기화. 따라서 lazyConfig.config map을 처음 초기화 할 때 내부의 print문이 동작.
+    println("this is main scope : ${lazyConfig.config["baseUrl"]}")
+
+    // NOTE : 두번째 사용시에는 리소스를 재로드하지않음. 캐싱된 값을 사용.
+    println("this is main scope : ${lazyConfig.config["apiKey"]}")
 }
