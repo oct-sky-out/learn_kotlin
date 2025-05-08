4b9e5a1ad177335dfef6d1bd4734f74e64cee709
oct-sky-out <kds3335k@icloud.com>
2025-05-08 16:55:43 +0900

    delete

--- src/main/kotlin/Axis.kt
+++
@@ -1,4 +0,0 @@
-package com.oct_sky_out
-
-// NOTE: data class는 기본적으로 equalAndHashCode 특성 및 ToString, copy(clone)객체에대한 정의를 가지고있다.
-data class Axis(val x : Int, val y : Int)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,4 @@
 package com.oct_sky_out
 
-fun colorTypeChecker(color: Color) : String {
-    return when (color) {
-        is Color.Black -> "black"
-        is Color.White -> "white"
-        is Color.Green -> "green"
-        is Color.Red -> "red"
-        is Color.Blue -> "blue"
-
-        // NOTE else 문 필요없음!!!
-    }
-}
-
 fun main() {
-    val xy1 = Axis(1, 23)
-    val xy2 = Axis(1, 23)
-
-    println("xy1 is eqaul xy2 = ${xy1 == xy2}")
-
-    println(Color.Green.getColorMap())
-    println(colorTypeChecker(Color.Black))
-
 }
