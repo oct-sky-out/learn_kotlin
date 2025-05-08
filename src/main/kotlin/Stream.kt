56d309846fb2601e4f561be97f2bf5613acc5ebc
oct-sky-out <kds3335k@icloud.com>
2025-05-08 22:25:53 +0900

    ch2 TODO 2번 작성

--- src/main/kotlin/Stream.kt
+++ src/main/kotlin/Stream.kt
@@ -12,5 +12,20 @@ class Stream() {
             // TODO 1 : v 함수를 계산(evaluation)하는 코드를 작성하세요.
             return ...
         }
+
+        fun <T> streamOf(x : T, y : T): Pair<T, () -> T> {
+            return Pair(x, delay(y))
+        }
+
+        fun <T> head(stream : Pair<T, *>) : T {
+            val (head, _) = stream
+            return head
+        }
+
+        fun <T> tail(stream : Pair<*, () -> T>) : T {
+            val (_, xs) = stream
+            // TODO 2 : tail 함수를 구현해주세요.
+            return ...
+        }
     }
 }
